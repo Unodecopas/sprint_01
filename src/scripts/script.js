a65f39e2369bbe68d3b1f7d6b476472c27a95193
German <72597058+GM-GitH@users.noreply.github.com>
2022-09-19 11:05:27 +0200

    update

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,4 @@
+//------------------------------------------------------ NAVBAR DROPDOWN
 const menu = document.querySelector(".navbar__menu");
 const menuItems = document.querySelectorAll(".navbar__menu__li");
 const burger = document.querySelector(".navbar__burger");
@@ -7,12 +8,12 @@ const menuIcon = document.querySelector(".navbar__burger__open");
 function toggleMenu() {
   if (menu.classList.contains("hidden")) {
     menu.classList.remove("hidden");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("hidden");
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
+  } else {
+    menu.classList.add("hidden");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
   }
 }
 
